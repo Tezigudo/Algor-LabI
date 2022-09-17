311b024562144ace95a1ba072abb83995249fc20
Tezigudo <godjangg@gmail.com>
2022-09-17 10:55:57 +0700

    added

--- Wk4/grader/add_brick.cpp
+++ Wk4/grader/add_brick.cpp
@@ -1,8 +1,64 @@
-#include<iostream>
-#include<cmath>
-#include<algorithm>
+#include <algorithm>
+#include <iostream>
+#include <tuple>
+#include <vector>
 using namespace std;
 
-int main(){
-    return 0;
+typedef vector<vector<int> > nested;
+
+tuple<int, int, int> compute_brick(int top, int bottom_left, int bottom_right) {
+  int bottom_mid = (top - bottom_left - bottom_right) / 2;
+  int mid_right = bottom_mid + bottom_right;
+  int mid_left = bottom_mid + bottom_left;
+
+  return {bottom_mid, mid_left, mid_right};
+}
+
+nested init_wall(int size) {
+  nested wall;
+  for (int i = 1; i <= size; i++) {
+    vector<int> brick;
+    for (int j = 0; j <= i-1; j++) {
+      brick.push_back(0);
+    }
+    wall.push_back(brick);
+    }
+  return wall;
+  }
+
+
+void print_wall(nested wall) {
+  cout << '[' << endl;
+  for (int i = 0; i < wall.size(); i++) {
+    cout << '[';
+    for (int j = 0; j < wall[i].size(); j++) {
+      cout << wall[i][j];
+      if (j!=wall[i].size()-1){
+        cout << ", ";
+      }
+    }
+    cout << "]";
+    if (i!=wall.size()-1){
+      cout << ',' << endl;
+    }else{
+      cout << endl << ']';
+    }
+  }
+}
+
+// void initialize_wall(int arr[9][9]) {
+//   for (int i = 0; i < 9; i++) {
+//     for (int j = 0; j < 9; j++) {
+//       arr[i][j] = 0;
+//     }
+//   }
+// }
+
+int main() {
+  //   int wall[9][9];
+  //   initialize_wall(wall);
+  nested small_wall = init_wall(9);
+  print_wall(small_wall);
+
+  return 0;
 }
--- Wk4/grader/fortune_cookies.cpp
+++ Wk4/grader/fortune_cookies.cpp
@@ -1,75 +1,92 @@
 #include <algorithm>
-#include <vector>
 #include <iostream>
 using namespace std;
 
-// struct cookie {
-//   int val;
-//   cookie* next;
-//   cookie(int value, cookie* next_cookie = NULL) {
-//     val = value;
-//     next = next_cookie;
-//   }
-// };
+struct cookie {
+  int val;
+  cookie* next;
+  cookie(int value, cookie* next_cookie = NULL) {
+    val = value;
+    next = next_cookie;
+  }
+};
 
-// cookie* find_kth_cookie(cookie* header, int k) {
-//   if (k != 0) {
-//     for (int i = 0; i < k; i++) {
-//       header = header->next;
-//     }
-//   }
-//   return header;
-// }
+cookie* find_kth_cookie(cookie* header, int k) {
+  if (k != 0) {
+    for (int i = -1; i < k-1 && header->next!=NULL; i++) {
+      header = header->next;
+    }
+  }
+  return header;
+}
 
-// void insert(cookie* header, int k, int val) {
-//   cookie* new_cookie = new cookie(val);
-//   while (k--) {
+void insert(cookie* header, int k, int val, int* count) {
+  cookie* new_cookie = new cookie(val);
+  cookie* head_cookie = find_kth_cookie(header, k);
+  new_cookie->next = head_cookie->next;
+  head_cookie->next = new_cookie;
+  *count+=1;
+}
+
+void eat(cookie* header, int k, int* count) {
+  if(header->next==NULL || *count <= k){
+    return;
+  }
+    cookie* head= find_kth_cookie(header, k);
+    cookie* temp = head->next;
+    head->next = temp->next;
+    delete temp;
+    *count -=1;
+}
+
+void print_cookie(cookie* header){
+    while(header->next!=NULL){
+        header = header->next;
+        cout << header->val << endl;
+    }
+}
+
+// void print_list_cookir(cookie* header){
+//   while(header->next!=NULL){
 //     header = header->next;
+//     cout << header->val << "->";
 //   }
-//   cookie* temp = header->next;
-//   header->next = new_cookie;
-//   new_cookie->next = temp;
+//   cout << endl;
 // }
-
-
-// void eat(cookie* header, int k) {
-//     cookie* prev = find_kth_cookie(header, k-1);
-//     cookie* next = prev->next->next;
-//     delete prev->next;
-//     prev->next = next;\
-// }
-
-// void print_cookie(cookie* header){
-//     while(header->next!=NULL){
-//         header = header->next;
-//         cout << header->val << "-> ";
-//     }
-// }
-
 int main() {
-  int T, operation;
-  // cookie* header = new cookie(0);
-  vector<int> cookies;
+  int T, operation, count=0;
+  cookie* header = new cookie(0);
   cin >> T;
   while (T--) {
     cin >> operation;
     switch (operation) {
         int k, num;
       case 1:
-        cin >> k >> num;
-        cookies.insert(cookies.begin()+k, num);
+      cin >> k >> num;
+      insert(header, k, num, &count);
         break;
 
       case 2:
       cin >> k;
-      if (cookies.size() >=k-1){
-        cookies.erase(cookies.begin()+k);
-      }
+      eat(header, k-1, &count);
         break;
     }
+    // print_list_cookir(header);
   }
-  for(auto itr=cookies.begin();itr!=cookies.end();itr++){
-    cout << *itr << endl;
-  }
+  print_cookie(header);
   return 0;
 }
+
+/*
+10
+2 3
+1 1 1
+1 0 3
+1 1 7
+1 3 29
+2 3
+1 0 90
+2 1
+1 5 100
+2 5
+*/
